b3b5abcad61fd309fd292ecf73458d0fa60095f3
Anatoli Zdanevich <anatoli.zdanevitch@gmail.com>
2024-01-19 19:02:30 +0300

    feat: add resolve for 02

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('node:path');
 
-const readStream = fs.createReadStream(path.join(__dirname, "text.txt"), {encoding: "utf-8"});
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), {
+  encoding: 'utf-8',
+});
 
-readStream.on("data", (chunk) => {
-  console.log(chunk)
-})
+readStream.on('data', (chunk) => {
+  console.log(chunk);
+});
---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+const fs = require('fs');
+const path = require('node:path');
+const writeStrem = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('hello stepan \n');
+
+rl.prompt();
+rl.on('line', (word) => {
+  if (word === 'exit') {
+    rl.setPrompt('thanks, goodbay');
+    rl.prompt();
+    rl.close();
+  } else {
+    writeStrem.write(word);
+    writeStrem.write('\n');
+  }
+});
+
+rl.on('SIGINT', () => {
+  rl.setPrompt('thanks, goodbay');
+  rl.prompt();
+  rl.close();
+});
